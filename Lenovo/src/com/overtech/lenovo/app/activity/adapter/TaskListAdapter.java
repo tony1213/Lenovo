49b9020175eacf69cff1c675aada2c6f8ee29ca6
zAndroidDeveloper <zAndroidDeveloper@163.com>
2016-03-09 17:59:05 +0800

    首页工单模拟数据展示

---
+++ Lenovo/src/com/overtech/lenovo/app/activity/adapter/TaskListAdapter.java
@@ -0,0 +1,110 @@
+package com.overtech.lenovo.app.activity.adapter;
+
+import java.util.List;
+import java.util.Random;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.overtech.lenovo.R;
+import com.overtech.lenovo.entity.tasklist.webservice.Task;
+import com.overtech.lenovo.picasso.Picasso;
+
+public class TaskListAdapter extends Adapter<TaskListAdapter.MyViewHolder> {
+	private Context ctx;
+	private List<Task> datas;
+
+	public TaskListAdapter(Context ctx, List<Task> datas) {
+		this.ctx = ctx;
+		this.datas = datas;
+	}
+
+	@Override
+	public int getItemCount() {
+		// TODO Auto-generated method stub
+		return datas.size();
+	}
+
+	@Override
+	public void onBindViewHolder(MyViewHolder holder, int position) {
+		// TODO Auto-generated method stub
+		Task task = datas.get(position);
+		if (new Random().nextFloat() > 0.5) {
+			Picasso.with(ctx).load(R.drawable.icon_tasklist_default1)
+					.into(holder.taskLogo);
+		} else {
+			Picasso.with(ctx).load(R.drawable.icon_tasklist_default2)
+					.into(holder.taskLogo);
+		}
+		holder.taskNo.setText(task.getTaskNo());
+		holder.taskDate.setText(task.getTaskDate());
+		holder.taskClass.setText(task.getTaskClass());
+		holder.taskDescription.setText(task.getTaskDescription());
+		holder.taskRemark.setText(task.getTaskRemark());
+		holder.taskDistance.setText("8km");
+		holder.taskVisitTime.setText(task.getTaskVisitTime());
+		if (task.getTaskType().equals("接单")) {
+			holder.taskReceive.setVisibility(View.VISIBLE);
+			holder.taskWaitEvaluate.setVisibility(View.GONE);
+		} else if (task.getTaskType().equals("评价")) {
+			holder.taskReceive.setVisibility(View.GONE);
+			holder.taskWaitEvaluate.setVisibility(View.VISIBLE);
+		}
+		if (task.getIsUrgent().equals("1")) {
+			holder.taskUrgency.setVisibility(View.VISIBLE);
+		} else if (task.getIsUrgent().equals("0")) {
+			holder.taskUrgency.setVisibility(View.GONE);
+		}
+
+	}
+
+	@Override
+	public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		// TODO Auto-generated method stub
+		MyViewHolder vh = new MyViewHolder(LayoutInflater.from(ctx).inflate(
+				R.layout.item_recyclerview_tasklist, parent, false));
+
+		return vh;
+	}
+
+	class MyViewHolder extends ViewHolder {
+		ImageView taskLogo;
+		TextView taskNo;
+		TextView taskDate;
+		TextView taskClass;
+		TextView taskDescription;
+		TextView taskRemark;
+		TextView taskDistance;
+		TextView taskVisitTime;
+		Button taskReceive;
+		TextView taskWaitEvaluate;
+		ImageView taskUrgency;
+
+		public MyViewHolder(View view) {
+			super(view);
+			// TODO Auto-generated constructor stub
+			taskLogo = (ImageView) view.findViewById(R.id.iv_task_logo);
+			taskNo = (TextView) view.findViewById(R.id.tv_task_no);
+			taskDate = (TextView) view.findViewById(R.id.tv_task_date);
+			taskClass = (TextView) view.findViewById(R.id.tv_task_class);
+			taskDescription = (TextView) view
+					.findViewById(R.id.tv_task_description);
+			taskRemark = (TextView) view.findViewById(R.id.tv_task_remark);
+			taskDistance = (TextView) view.findViewById(R.id.tv_task_distance);
+			taskVisitTime = (TextView) view
+					.findViewById(R.id.tv_task_visit_time);
+			taskReceive = (Button) view.findViewById(R.id.bt_task_receive);
+			taskWaitEvaluate = (TextView) view
+					.findViewById(R.id.tv_task_wait_evaluate);
+			taskUrgency = (ImageView) view.findViewById(R.id.iv_task_urgency);
+		}
+
+	}
+}
--- Lenovo/src/com/overtech/lenovo/app/activity/fragment/TasklistFragment.java
+++ Lenovo/src/com/overtech/lenovo/app/activity/fragment/TasklistFragment.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ItemDecoration;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -11,7 +14,9 @@ import android.widget.TextView;
 
 import com.overtech.lenovo.R;
 import com.overtech.lenovo.app.BaseFragment;
+import com.overtech.lenovo.app.activity.adapter.TaskListAdapter;
 import com.overtech.lenovo.entity.tasklist.webservice.ADInfo;
+import com.overtech.lenovo.entity.tasklist.webservice.Task;
 import com.overtech.lenovo.utils.Utilities;
 import com.overtech.lenovo.widget.bitmap.ImageLoader;
 import com.overtech.lenovo.widget.cycleviewpager.CycleViewPager;
@@ -24,6 +29,9 @@ public class TasklistFragment extends BaseFragment implements OnClickListener {
 	private TextView mTaskVisit;
 	private TextView mTaskAccount;
 	private TextView mTaskEvaluation;
+	private RecyclerView mRecyclerView;
+	private TaskListAdapter adapter;
+	private List<Task> datas;
 	private CycleViewPager cycleViewPager;
 	private List<ImageView> views = new ArrayList<ImageView>();
 	private List<ADInfo> infos = new ArrayList<ADInfo>();
@@ -50,15 +58,46 @@ public class TasklistFragment extends BaseFragment implements OnClickListener {
 		mTaskAccount = (TextView) mRootView.findViewById(R.id.tv_task_account);
 		mTaskEvaluation = (TextView) mRootView
 				.findViewById(R.id.tv_task_evaluation);
-
+		mRecyclerView = (RecyclerView) mRootView
+				.findViewById(R.id.recyclerView);
 		initialCycleViewPager();
-
+		initRecyclerView();
 		mTaskAll.setOnClickListener(this);
 		mTaskReceive.setOnClickListener(this);
 		mTaskOrder.setOnClickListener(this);
 		mTaskVisit.setOnClickListener(this);
 		mTaskAccount.setOnClickListener(this);
 		mTaskEvaluation.setOnClickListener(this);
+	}
+
+	private void initRecyclerView() {
+		// TODO Auto-generated method stub
+		mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+		mRecyclerView.addItemDecoration(new ItemDecoration() {});//实现分割线
+		datas = new ArrayList<Task>();
+		Task task1 = new Task("", "20160309-0001", "2016-03-11  11:30", "网络问题",
+				"WIFI无法正常使用", "携带装备", "langitude", "longitude", "2小时上门", "1",
+				"接单");
+		Task task2 = new Task("", "20160309-0002", "2016-03-12  12:30", "POS软件问题",
+				"pos无法正常使用", "携带装备", "langitude", "longitude", "6小时上门", "0",
+				"评价");
+		Task task3 = new Task("", "20160309-0003", "2016-03-13  13:30", "网络问题",
+				"哈哈无法正常使用", "携带装备", "langitude", "longitude", "2小时上门", "0",
+				"接单");
+		Task task4 = new Task("", "20160309-0004", "2016-03-14  14:30", "网络问题",
+				"pos无法正常使用", "携带装备", "langitude", "longitude", "3小时上门", "1",
+				"评价");
+		Task task5 = new Task("", "20160309-0005", "2016-03-15  15:30", "网络问题",
+				"设备无法正常使用", "携带装备", "langitude", "longitude", "4小时上门", "0",
+				"接单");
+
+		datas.add(task1);
+		datas.add(task2);
+		datas.add(task3);
+		datas.add(task4);
+		datas.add(task5);
+		adapter = new TaskListAdapter(getActivity(), datas);
+		mRecyclerView.setAdapter(adapter);
 	}
 
 	private void initialCycleViewPager() {
