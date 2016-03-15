35704c5e4d0618ab9e6c792888eef647fe715064
zAndroidDeveloper <zAndroidDeveloper@163.com>
2016-03-15 15:07:34 +0800

    本单详情——本单信息Demo完成

--- Lenovo/src/com/overtech/lenovo/app/activity/adapter/TaskInfoFragAdapter.java
+++ Lenovo/src/com/overtech/lenovo/app/activity/adapter/TaskInfoFragAdapter.java
@@ -2,19 +2,21 @@ package com.overtech.lenovo.app.activity.adapter;
 
 import java.util.List;
 
-import com.overtech.lenovo.R;
-
 import android.content.Context;
-import android.view.LayoutInflater;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.overtech.lenovo.R;
+import com.overtech.lenovo.entity.tasklist.webservice.TaskProcess;
 
 public class TaskInfoFragAdapter extends BaseAdapter {
 	private Context ctx;
-	private List datas;
+	private List<TaskProcess> datas;
 
-	public TaskInfoFragAdapter(Context ctx, List datas) {
+	public TaskInfoFragAdapter(Context ctx, List<TaskProcess> datas) {
 		this.ctx = ctx;
 		this.datas = datas;
 	}
@@ -28,7 +30,7 @@ public class TaskInfoFragAdapter extends BaseAdapter {
 	@Override
 	public Object getItem(int arg0) {
 		// TODO Auto-generated method stub
-		return null;
+		return datas.get(arg0);
 	}
 
 	@Override
@@ -50,12 +52,37 @@ public class TaskInfoFragAdapter extends BaseAdapter {
 		} else {
 			vh = (ViewHolder) convertView.getTag();
 		}
+		TaskProcess task = datas.get(position);
+		vh.mTaskState.setText(task.getState());
+		vh.mTaskTime.setText(task.getTime());
+		if (task.getAction().equals("")) {
+			vh.mButton.setVisibility(View.GONE);
+		} else {
+			vh.mButton.setVisibility(View.VISIBLE);
+			vh.mButton.setText(task.getAction());
+		}
+		if (task.getOther().equals("")) {
+			vh.mOther.setVisibility(View.GONE);
+		} else {
+			vh.mOther.setVisibility(View.VISIBLE);
+			vh.mOther.setText(task.getOther());
+		}
+
 		return convertView;
 	}
 
 	class ViewHolder {
-		public ViewHolder(View view) {
+		TextView mTaskState;
+		TextView mTaskTime;
+		TextView mOther;
+		AppCompatButton mButton;
 
+		public ViewHolder(View view) {
+			mTaskState = (TextView) view
+					.findViewById(R.id.tv_task_process_state);
+			mTaskTime = (TextView) view.findViewById(R.id.tv_task_process_time);
+			mOther = (TextView) view.findViewById(R.id.tv_task_process_other);
+			mButton = (AppCompatButton) view.findViewById(R.id.bt_task_action);
 		}
 	}
 
--- Lenovo/src/com/overtech/lenovo/app/activity/task/fragment/TaskInformationFragment.java
+++ Lenovo/src/com/overtech/lenovo/app/activity/task/fragment/TaskInformationFragment.java
@@ -9,11 +9,13 @@ import android.widget.ListView;
 import com.overtech.lenovo.R;
 import com.overtech.lenovo.app.BaseFragment;
 import com.overtech.lenovo.app.activity.adapter.TaskInfoFragAdapter;
+import com.overtech.lenovo.entity.tasklist.webservice.TaskProcess;
 
 public class TaskInformationFragment extends BaseFragment {
 	private ListView mTaskProcess;
 	private TaskInfoFragAdapter adapter;
-	private List datas;
+	private List<TaskProcess> datas;
+
 	@Override
 	protected int getLayoutId() {
 		// TODO Auto-generated method stub
@@ -23,15 +25,15 @@ public class TaskInformationFragment extends BaseFragment {
 	@Override
 	protected void afterCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		mTaskProcess=(ListView) mRootView.findViewById(R.id.lv_task_process);
-		datas=new ArrayList();
-		datas.add("1");
-		datas.add("2");
-		datas.add("3");
-		datas.add("4");
-		datas.add("5");
-		datas.add("6");
-		adapter=new TaskInfoFragAdapter(getActivity(), datas);
+		mTaskProcess = (ListView) mRootView.findViewById(R.id.lv_task_process);
+		datas = new ArrayList<TaskProcess>();
+		datas.add(new TaskProcess("开单", "2016/01/22", "单号：20160122-0008", ""));
+		datas.add(new TaskProcess("接单", "", "", "接单"));
+		datas.add(new TaskProcess("预约", "2016/01/22 10:50", "", "改约"));
+		datas.add(new TaskProcess("到场", "2016/01/22 11:30", "", "到场"));
+		datas.add(new TaskProcess("完成", "2016/01/22 12:00", "", "解决方案"));
+		datas.add(new TaskProcess("评价", "2016/01/22 14:00", "问题已经解决，态度认真负责", ""));
+		adapter = new TaskInfoFragAdapter(getActivity(), datas);
 		mTaskProcess.setAdapter(adapter);
 	}
 
