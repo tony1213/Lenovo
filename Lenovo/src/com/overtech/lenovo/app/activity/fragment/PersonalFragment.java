6d91a1b6da3dfa8f87ab17f7fdc6de8e7a03abd8
zAndroidDeveloper <zAndroidDeveloper@163.com>
2016-03-22 13:25:27 +0800

    采用CollapsingLayout画个人设置UI

--- Lenovo/src/com/overtech/lenovo/app/activity/fragment/PersonalFragment.java
+++ Lenovo/src/com/overtech/lenovo/app/activity/fragment/PersonalFragment.java
@@ -1,20 +1,24 @@
 package com.overtech.lenovo.app.activity.fragment;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.overtech.lenovo.R;
 import com.overtech.lenovo.app.BaseFragment;
-import com.overtech.lenovo.picasso.Picasso;
+import com.overtech.lenovo.app.activity.personal.PersonalSettingActivity;
 import com.overtech.lenovo.picasso.Transformation;
 import com.overtech.lenovo.utils.ImageCacheUtils;
 import com.overtech.lenovo.widget.bitmap.ImageLoader;
 
-public class PersonalFragment extends BaseFragment {
+public class PersonalFragment extends BaseFragment implements OnClickListener {
 	private ImageView mAvator;
+	private LinearLayout setting;
 	private String imageUrl = "http://img0w.pconline.com.cn/pconline/1309/03/3452566_13.jpg";
 
 	@Override
@@ -27,6 +31,8 @@ public class PersonalFragment extends BaseFragment {
 	protected void afterCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		mAvator = (ImageView) mRootView.findViewById(R.id.iv_avator);
+		setting=(LinearLayout)mRootView.findViewById(R.id.ll_personal_setting);
+		
 		ImageLoader.getInstance().displayImage(imageUrl, mAvator,
 				R.drawable.icon_avator_default, R.drawable.ic_launcher,
 				new Transformation() {
@@ -43,5 +49,22 @@ public class PersonalFragment extends BaseFragment {
 						return null;
 					}
 				}, Config.RGB_565);
+		
+		setting.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.ll_personal_setting:
+			Intent intent = new Intent(getActivity(),PersonalSettingActivity.class);
+			startActivity(intent);
+			
+			break;
+
+		default:
+			break;
+		}
 	}
 }
